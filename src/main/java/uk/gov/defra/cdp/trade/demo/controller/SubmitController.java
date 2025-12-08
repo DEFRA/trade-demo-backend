f60b9199a48401fabb76e99322d55ffdcb40c184
tarun.kumar <tarun.kumar@defra.onmicrosoft.com>
2025-12-08 11:54:52 +0000

    INSC-46 sts api client token generation

--- src/main/java/uk/gov/defra/cdp/trade/demo/controller/SubmitController.java
+++ src/main/java/uk/gov/defra/cdp/trade/demo/controller/SubmitController.java
@@ -3,6 +3,7 @@ package uk.gov.defra.cdp.trade.demo.controller;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +14,13 @@ import uk.gov.defra.cdp.trade.demo.service.WebIdentityTokenService;
 @Slf4j
 @RequestMapping("/ipaffs")
 @RestController
-@AllArgsConstructor
-@Profile({"!integration-test"})
 public class SubmitController {
     
     private final WebIdentityTokenService webIdentityTokenService;
+    
+    public SubmitController(WebIdentityTokenService webIdentityTokenService) {
+        this.webIdentityTokenService = webIdentityTokenService;
+    }
 
     @GetMapping("/submit")
     public ResponseEntity<String> submit() {
