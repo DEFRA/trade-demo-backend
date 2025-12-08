a05dd461ce04c17306ad2613166aedecc68aaab1
TARUN KUMAR PALISETTY <tarun.mca@gmail.com>
2025-12-08 15:13:45 +0000

    INSC-46 sts api client token generation (#37)
    Co-authored-by: tarun.kumar <tarun.kumar@defra.onmicrosoft.com>

--- src/main/java/uk/gov/defra/cdp/trade/demo/controller/SubmitController.java
+++ src/main/java/uk/gov/defra/cdp/trade/demo/controller/SubmitController.java
@@ -1,28 +1,30 @@
 package uk.gov.defra.cdp.trade.demo.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.defra.cdp.trade.demo.configuration.AwsConfig;
+import uk.gov.defra.cdp.trade.demo.service.WebIdentityTokenService;
 
 @Slf4j
 @RequestMapping("/ipaffs")
 @RestController
+@AllArgsConstructor
+@Profile({"!integration-test"})
 public class SubmitController {
     
-    private final AwsConfig awsConfig;
-    
-    public SubmitController(AwsConfig awsConfig) {
-        this.awsConfig = awsConfig;
-    }
+    private final WebIdentityTokenService webIdentityTokenService;
 
     @GetMapping("/submit")
     public ResponseEntity<String> submit() {
         log.info("SUBMIT /notifications");
 
-        String token = awsConfig.webIdentityToken();
+        String token = webIdentityTokenService.getWebIdentityToken();
+        log.info("Using cached STS token: {}", token.substring(0 ,20));
 
         return ResponseEntity.ok(token);
     }
